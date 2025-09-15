ddffe284ed798d25fec775b670de8ff38897c062
ErwinBezu <erwin.bezu@gmail.com>
2025-09-15 16:06:34 +0200

    exo 5 finito

---
+++ src/Exo5.kt
@@ -0,0 +1,18 @@
+fun main () {
+    println(compterLettreA("C'est le b-a ba"))
+    println(compterLettreA("mixer"))
+}
+
+fun compterLettreA(texte: String) = texte.count { it == 'a' || it == 'A' }
+
+/* Version longue (je préfère la version one liner xD)
+
+fun compterLettreA (texte : String) : Int {
+    var compteur = 0
+    for (lettre in texte) {
+        if (lettre == 'a' || lettre == 'A') {
+            compteur++
+        }
+    }
+    return compteur
+} */
